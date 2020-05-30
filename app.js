448f0dfe140e4c51ddaee85c1565c72d891d84fe
eben-k <ennim.consults@gmail.com>
2020-05-30 20:04:00 +0100

    create data sructure for budget controller

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 // Budget controller
-const budgetController = (function () {})();
+const budgetController = (function () {
+  const Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, des, val) {
+      let newItem;
+      let ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp'
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+
+      // add it to datastructure
+      data.allItems[type].push(newItem);
+
+      // return new item
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
+    },
+  };
+})();
 
 // UI controller
 const UIController = (function () {
@@ -43,6 +96,11 @@ const controller = (function (budgetCtrl, uiCntrl) {
     // get the filled input data
     const input = uiCntrl.getInput();
     // add the item to the budget controller
+    const newItem = budgetCtrl.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
     // add the item to the ui
     // calculate the budget
     // display budget on the ui
